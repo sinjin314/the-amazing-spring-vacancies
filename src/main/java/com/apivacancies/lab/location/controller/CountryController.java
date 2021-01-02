cb5e3fd7f84081ce5953946037acc8467dae4977
abdelhadi hasnaoui <abdelhdai.hasnaoui@protonmail.com>
2021-01-02 21:59:54 +0100

    populating database, and maping idea

--- src/main/java/com/apivacancies/lab/location/controller/CountryController.java
+++ src/main/java/com/apivacancies/lab/location/controller/CountryController.java
@@ -11,10 +11,13 @@ import com.apivacancies.lab.location.service.CountryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RestController("/rest/api/country")
@@ -29,7 +32,23 @@ public class CountryController {
     @ResponseStatus(HttpStatus.OK)
     public List<Country> getCountrys()
     {
-        return countryService.getCountry();
+        return countryService.getCountrys();
     }
 
+    @GetMapping("/country/generate")
+    @ResponseStatus(HttpStatus.OK)
+    public void generateCountrys() {
+        countryService.generateCountrys();
+    }
+
+    @GetMapping("/country/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Country> getCountry(@PathVariable Long id)
+    {
+        Optional<Country> country = countryService.getCountry(id);
+        if(!country.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Country doesn't exist in database");
+        }
+        return country;
+    }
 }
--- src/main/java/com/apivacancies/lab/location/service/CountryService.java
+++ src/main/java/com/apivacancies/lab/location/service/CountryService.java
@@ -11,6 +11,7 @@ import com.apivacancies.lab.location.repository.CountryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
@@ -21,8 +22,42 @@ public class CountryService {
             this.countryRepository = countryRepository;
         }
 
-        public List<Country> getCountry() {
+        public List<Country> getCountrys() {
             return countryRepository.findAll();
+        }
+
+        public Optional<Country> getCountry(long id) {
+            return countryRepository.findById(id);
+        }
+
+        public void generateCountrys() {
+            Country country = new Country();
+            country.setName("France");
+            countryRepository.save(country);
+
+            country = new Country();
+            country.setName("Algeria");
+            countryRepository.save(country);
+
+            country = new Country();
+            country.setName("Canada");
+            countryRepository.save(country);
+
+            country = new Country();
+            country.setName("Brasil");
+            countryRepository.save(country);
+
+            country = new Country();
+            country.setName("Japan");
+            countryRepository.save(country);
+
+            country = new Country();
+            country.setName("Vietnam");
+            countryRepository.save(country);
+
+            country = new Country();
+            country.setName("Wakanda");
+            countryRepository.save(country);
         }
 
 }
